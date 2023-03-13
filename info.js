48b1006c57ae950df7a886f2fdd098464c3aea3e
BOTNue <joshua@fourie.se>
2023-03-13 09:51:30 +0100

    I did something

---
+++ info.js
@@ -0,0 +1,12 @@
+const infojoshua = {
+    name: "Joshua Fourie",
+    birthday: "4 aug", 
+    city: "stockholm",
+    occupations: ["Student", "Dålig programmerare", "professional fps chess player"]
+}
+
+function getname(){
+    return infojoshua.name;
+}
+
+console.log(getname)
